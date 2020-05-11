1aadea43ca2ddb987130cb02631920c3d0bffc86
ilchanJoo <ilchanjoo@gmail.com>
2020-05-11 17:03:06 +0900

    인프런 - 뷰스타트

--- 인프런/MyApp/Controllers/HomeController.cs
+++ 인프런/MyApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyApp.Models;
+using MyApp.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,14 +13,32 @@ namespace MyApp.Controllers
     public class HomeController : Controller
     {
         // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
         public IActionResult Student()
         {   // Razor 파일 디스플레이
-            return View();
+
+            List<Teacher> teachers = new List<Teacher>()
+            {
+                new Teacher() {Name ="세종대왕", Class="한글"},
+                new Teacher() {Name ="이순신", Class="해상전략"},
+                new Teacher() {Name ="제갈량", Class="지략"},
+                new Teacher() {Name ="을지문덕", Class="지상전략"},
+            };
+
+            var viewModel = new StudentTeacherViewModels()
+            {
+                Student = new Student(),
+                Teachers = teachers
+            };
+            return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Student(Student model)
+        public IActionResult Student(StudentTeacherViewModels model)
         {   // View에서 넘어오는 값들을 받음
             if (ModelState.IsValid)
             {
