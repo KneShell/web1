f536a96ff266464f9c4d8d7ba10cef58c3c75504
ilchanJoo <ilchanjoo@gmail.com>
2020-05-13 14:08:54 +0900

    인프런 CRUD_read

--- 인프런/MyApp/Controllers/HomeController.cs
+++ 인프런/MyApp/Controllers/HomeController.cs
@@ -39,18 +39,12 @@ namespace MyApp.Controllers
         public IActionResult Student()
         {   // Razor 파일 디스플레이
 
-            List<Teacher> teachers = new List<Teacher>()
-            {
-                new Teacher() {Name ="세종대왕", Class="한글"},
-                new Teacher() {Name ="이순신", Class="해상전략"},
-                new Teacher() {Name ="제갈량", Class="지략"},
-                new Teacher() {Name ="을지문덕", Class="지상전략"},
-            };
+            var students = _studentRepository.GetAllStudent();
 
             var viewModel = new StudentTeacherViewModels()
             {
                 Student = new Student(),
-                Teachers = teachers
+                Students = students
             };
             return View(viewModel);
         }
@@ -65,7 +59,23 @@ namespace MyApp.Controllers
                 _studentRepository.AddStudent(model.Student);
                 _studentRepository.Save();
             }
-            return View();
+
+            var students = _studentRepository.GetAllStudent();
+
+            var viewModel = new StudentTeacherViewModels()
+            {
+                Student = new Student(),
+                Students = students
+            };
+
+            ModelState.Clear();
+            return View(viewModel);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var result = _studentRepository.GetStudent(id);
+            return View(result);
         }
     }
 }
